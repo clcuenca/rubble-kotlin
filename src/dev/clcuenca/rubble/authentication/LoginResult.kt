cfb84eb1b36cefe76428b1a54d31edf7a35dd0b5
Carlos Cuenca <cuenca.cl@gmail.com>
2020-08-17 20:51:35 -0700

    remove jackson annotations

--- src/dev/clcuenca/rubble/authentication/LoginResult.kt
+++ src/dev/clcuenca/rubble/authentication/LoginResult.kt
@@ -1,18 +1,9 @@
 package dev.clcuenca.rubble.authentication
 
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement
-
-@JacksonXmlRootElement
 class LoginResult {
 
-    @JacksonXmlProperty(isAttribute = true)
     var token            : String = ""
-
-    @JacksonXmlProperty(isAttribute = true)
     var registrationTime : String = ""
-
-    @JacksonXmlProperty(isAttribute = true)
     var timeToLive       : Long = 0L
 
 }
